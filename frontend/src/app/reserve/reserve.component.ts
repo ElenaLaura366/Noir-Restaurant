39ada9e21d2c0e7f31e07d53aa8308dc5abe1433
Garcineanu Elena-Laura <lauraelena366@gmail.com>
2025-01-13 10:44:39 +0200

    Enhance reservation component with form validation and footer integration; update layout and styling

--- frontend/src/app/reserve/reserve.component.ts
+++ frontend/src/app/reserve/reserve.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms'; 
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-reserve',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule, FooterComponent],
   templateUrl: './reserve.component.html',
   styleUrl: './reserve.component.css'
 })
 export class ReserveComponent {
+  contactForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log('Formular trimis:', this.contactForm.value);
+      alert('Mesaj trimis cu succes!');
+      this.contactForm.reset();
+    } else {
+      console.log('Formular invalid');
+    }
+  }
 }
